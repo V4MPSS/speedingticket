6bbaf5ce5ccbdd024e7b5fee9884a359d313480d
V4MPSS <159195405+V4MPSS@users.noreply.github.com>
2024-03-06 15:12:49 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+// Functionize Caught Speeding Start Code
+
+// Determine Tickets Button Click Event
+document.getElementById("btn").addEventListener("click", btnClicked);
+
+function btnClicked() {
+  // Get Speed & Speed Limit Inputs
+  let d1Speed = +document.getElementById("d1-speed").value;
+  let d1Limit = +document.getElementById("d1-limit").value;
+  let d2Speed = +document.getElementById("d2-speed").value;
+  let d2Limit = +document.getElementById("d2-limit").value;
+  let d3Speed = +document.getElementById("d3-speed").value;
+  let d3Limit = +document.getElementById("d3-limit").value;
+
+  // Calculate & Output Ticket Types
+  // Driver 1
+
+  document.getElementById("d1-ticket").innerHTML = calcticket(d1Speed, d1Limit);
+
+  // Driver 2
+
+  document.getElementById("d2-ticket").innerHTML = calcticket(d2Speed, d2Limit);
+
+  // Driver 3
+
+  document.getElementById("d3-ticket").innerHTML = calcticket(d3Speed, d3Limit);
+}
+function calcticket(speed, speedlim) {
+  let Ticket;
+  if (speed > speedlim + 40) {
+    Ticket = "Really Big Ticket";
+  } else if (speed > speedlim + 20) {
+    Ticket = "Big Ticket";
+  } else if (speed > speedlim) {
+    Ticket = "Small Ticket";
+  } else {
+    Ticket = "No Ticket";
+  }
+  return Ticket;
+}
